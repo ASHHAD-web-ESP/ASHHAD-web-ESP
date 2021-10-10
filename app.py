e9b8ff3bc591426c4a41a43f23a503a6f65cd211
ASHHAD-web-ESP <91800067+ASHHAD-web-ESP@users.noreply.github.com>
2021-10-10 17:28:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
-from flask import Flask,request, url_for, redirect, render_template, jsonify
+from flask import Flask, render_template
 from flask_socketio import SocketIO
-from flask_executor import Executor
-import pandas as pd
 import json
-
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-thread = None
 
-@app.route('/')
-def home():
-    return render_template("home.html")
+@app.route("/")
+def index():
+  return render_template('index.html',)
 
+@socketio.on('send_message')
+def handle_source(json_data):
+  text = json_data['message'].encode('ascii', 'ignore')
+  socketio.emit('echo', {'echo': 'Server Says: '+text})
 
-@socketio.on("response_demo")
-def background_task_func():
-    """Example of how to send server generated events to clients."""
-
-    socketio.sleep(5)
-    print("send")
-   
-    data = {'Name': ['Tom', 'Joseph', 'Krish', 'John','Shadz'], 'Age': [20, 21, 19, 18,36]} 
-
-    data_2= pd.DataFrame(data)
-    
-    df_json=data_2.to_json(orient='records')
-    result = {"objects": json.loads(df_json)}
-    socketio.emit('my_response',result, broadcast=True)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    executor = Executor(app)
-    socketio.run(app ,port=5000 )
+if __name__ == "__main__":
+  socketio.run(app)
